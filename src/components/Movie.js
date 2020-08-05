178cbbb33864b54bd9423c682dc07bdec45341ad
Sava <sava.paroski021@gmail.com>
2020-08-05 16:54:01 +0200

    Created movie component.

---
+++ src/components/Movie.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Movie(props) {
+
+  return (
+    <tr>
+      <th scope="row">{props.movieDetails.Title !== undefined ? props.movieDetails.Title : ''}</th>
+      <td>{props.movieDetails.Year !== undefined ? props.movieDetails.Year : ''}</td>
+      <td>{props.movieDetails.Genre !== undefined ? props.movieDetails.Genre : ''}</td>
+      <td>{props.movieDetails.Plot !== undefined ? props.movieDetails.Plot : ''}</td>
+      <td><img src={props.movieDetails.Poster} alt="" /></td>
+    </tr>
+  )
+}
